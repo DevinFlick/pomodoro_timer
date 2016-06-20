1d3acc46dff2eaf29a9907368e7470d0d77546da
DevinFlick <DevinJFlick@gmail.com>
2016-06-20 19:56:32 -0400

    user story 5 worked on

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log('My JavaScript is working...!!!!');
+$(document).ready(function(){
+  var start= $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+
+  start.on('click', startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal = seconds.text();
+      if(secondsVal === 0){
+}
+        else{
+          seconds.text(59);
+        }
+
+    }, 1000);
+  }
+});
