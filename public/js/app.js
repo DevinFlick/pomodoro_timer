623b50f5a711d388884ee883bdda7b9fd057461f
DevinFlick <DevinJFlick@gmail.com>
2016-06-23 19:31:27 -0400

    set up deployment to Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,23 +5,35 @@ $(document).ready(function(){
   var breakBtn = $('#break');
 
   start.on('click', startCountdown);
+  breakBtn.on('click', takeABreak);
+
+  function takeABreak(){
+    minutes.text('05');
+    seconds.text('00');
+    startCountdown();
+  }
 
   function startCountdown(){
-    setInterval(function(){
+    var countdown = setInterval(function(){
       var secondsVal = +seconds.text();
+      //plus sign makes the code treat the value as a number if at all possible
       var minutesVal = +minutes.text();
-      if (minutesVal === 0 && minutesVal === 0){
+        if (minutesVal === 0 && minutesVal === 0){
         breakBtn.removeClass('disabled');
         breakBtn.removeAttr('disabled');
+        clearInterval(countdown);
+        return;
       }
       if(secondsVal === 0){
+        minutes.text(minutesVal - 1);
         seconds.text(59);
       } else {
         if(secondsVal <= 10){
           seconds.text("0" + (secondsVal-1));
-      } else {
-        seconds.text(secondsVal -1);        }
+        } else {
+          seconds.text(secondsVal -1);
         }
+      }
 
     }, 1000);
   }
